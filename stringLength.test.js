2140c128eb07f57838f5078fbe27570c0912d680
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-09 14:15:22 +0630

    Finish stringLength testing and function

---
+++ stringLength.test.js
@@ -0,0 +1,14 @@
+const stringLength = require('./stringLength');
+
+describe('stringLength', () => {
+  it('should return true if the string is at least 1 character long and not longer than 10 characters', () => {
+    expect(stringLength('a')).toBeGreaterThan(0);
+    expect(stringLength('abcdefghi')).toBeLessThanOrEqual(10);
+    expect(stringLength('012345678')).toBeLessThanOrEqual(10);
+  });
+
+  it('should throw an error if the string is not one character or more than 10 characters', () => {
+    expect(() => stringLength('')).toThrow('The String has no characters');
+    expect(() => stringLength('abcdefghijs')).toThrow('The string is more than 10 characters');
+  });
+});
